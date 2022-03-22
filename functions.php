33b3d4e19fdb34e6b5d4fd3f5a1e172a5c5f7142
Pedro Silva <pedro@dig.id>
2022-03-22 11:43:48 +0000

    first commit

---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Setup theme
+ */
+function bocco_theme_setup() {
+
+	register_nav_menus(
+		array(
+			'primary' => __( 'Primary Menu', 'bocco' ),
+		)
+	);
+
+	add_theme_support( 'menus' );
+
+	add_theme_support( 'custom-logo' );
+
+	add_theme_support( 'title-tag' );
+
+	add_theme_support( 'post-thumbnails' );
+
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
+
+	add_image_size( 'custom', 1920, 900, array( 'center', 'center' ) );
+
+}
+
+add_action( 'after_setup_theme', 'bocco_theme_setup' );
+
+/**
+ * Enqueue styles and scripts
+ */
+function bocco_theme_enqueue_styles() {
+
+	$the_theme     = wp_get_theme();
+	$theme_version = $the_theme->get( 'Version' );
+
+	if ( ! is_admin() ) :
+		wp_enqueue_style( 'theme-styles', get_stylesheet_directory_uri() . '/dist/main.css', array(), $theme_version );
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'theme-scripts', get_stylesheet_directory_uri() . '/dist/main.js', array( 'jquery' ), $theme_version, false );
+	endif;
+
+}
+
+add_action( 'wp_enqueue_scripts', 'bocco_theme_enqueue_styles' );
