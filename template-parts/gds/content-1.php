f91a9a0f1c5a3eadca47645e7a9011778f8a9192
Daniel-Digid <daniel@dig.id>
2022-03-31 15:19:15 +0100

    Dev

---
+++ template-parts/gds/content-1.php
@@ -0,0 +1,26 @@
+<section id="gdscontent-1" class="section section-gdscontent-1">
+    <img class="section-gdscontent-1__img-banner" src="<?php echo wp_upload_dir()['url'] . '/GDS_A_1.svg' ?>">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-6 col-md-12 col-lg-6 col-xl-6 section-gdscontent-1__leftcol">
+                <div class="section-gdscontent-1__img-pack">
+                    <img class="section-gdscontent-1__img-logo1" src="<?php echo wp_upload_dir()['url'] . '/Logo_Sabre.svg' ?>">
+                    <img class="section-gdscontent-1__img-logo2" src="<?php echo wp_upload_dir()['url'] . '/Logo_Pegasus.svg' ?>">
+                    <img class="section-gdscontent-1__img-logo3" src="<?php echo wp_upload_dir()['url'] . '/Logo_Amadeus.svg' ?>">
+                </div>
+			</div>
+			<div class="col-6 col-md-12 col-lg-6 col-xl-6 section-gdscontent-1__rightcol">
+                <div class="section-gdscontent-1__content-wrapper">
+                    <p class="section-gdscontent-1__subtitle">Insert Overline title</p>
+                    <h1 class="section-gdscontent-1__title">Vorteile unserer<br>Channel Manager</h1>
+                    <ul class="section-gdscontent-1__list">
+                        <li>Mehr Sichtbarkeit auf dem Markt</li>
+                        <p>Durch eine Anbindung am Globalen Distribution System (GDS) erhalten Tour Operator und grosse Unternehmen die Möglichkeit online Zimmer bei Ihnen zu buchen.</p>
+                        <li>Automatisierung von TO und Corporate Buchungen</li>
+                        <p>Nie mehr manuelle Anfragen von Tour Operator bearbeiten? Dies ist dank einer Anbindung am GDS möglich. Die TO buchen hier Ihr Zimmer ganz einfach online und die Reservationen werden automatisch an Sie übermittelt.</p>
+                    </ul>
+                </div>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ template-parts/gds/intro.php
@@ -0,0 +1,18 @@
+<section id="gdsintro" class="section section-gdsintro">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-7 col-md-12 col-lg-7 col-xl-7 section-gdsintro__leftcol">
+                <div class="section-gdsintro__content-wrapper">
+                    <p class="section-gdsintro__subtitle">GDS</p>
+                    <h1 class="section-gdsintro__title">Globales Distributions System</h1>
+                    <p class="section-gdsintro__text">Ein Drittel des Reiseverkehrs läuft über GDS, welcher jährlich um 5 Prozent steigt. Mit unseren Möglichkeiten sind Ihre Angebote mit kleinem Aufwand in den Globalen Distributions Systemen (GDS) buchbar.</p>
+                </div>
+			</div>
+			<div class="col-5 col-md-12 col-lg-5 col-xl-5 section-gdsintro__rightcol">
+                <div class="section-gdsintro__img-pack">
+                    <img src="<?php echo wp_upload_dir()['url'] . '/GDS_O_1.png' ?>">
+                </div>
+			</div>
+		</div>
+	</div>
+</section>
