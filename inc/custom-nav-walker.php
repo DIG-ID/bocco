af0e66e6942b026619dcc02dff244253abaf0da2
Daniel-Digid <daniel@dig.id>
2022-04-07 12:02:38 +0100

    Dev

--- inc/custom-nav-walker.php
+++ inc/custom-nav-walker.php
@@ -185,11 +185,11 @@ if ( ! class_exists( 'Custom_Walker_Nav_Menu' ) ) :
 
 			if ( $menu_item->menu_item_parent > 0 ) :
 				$item_output  = $args->before;
-				$item_output .= '<a class="sub-menu__title" ' . $attributes . '>' . $title . '</a>';
+				$item_output .= '<a' . $attributes . '><div class="sub-menu__title" ' . $attributes . '>' . $title . '</div>';
 				$item_output .= '<p class="sub-menu__description">' . $menu_item->description . '</p>';
-				$item_output .= '<a' . $attributes . '>';
+				$item_output .= '<div' . $attributes . '>';
 				$item_output .= $args->link_before . __( 'Mehr Erfahren ➔', 'bocco-group' ) . $args->link_after;
-				$item_output .= '</a>';
+				$item_output .= '</div></a>';
 				$item_output .= $args->after;
 			else:
 				$item_output  = $args->before;
