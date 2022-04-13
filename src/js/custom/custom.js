3479334d91c43535fc0b0646a45c9f7b2b632b35
pedro-digid <pedro@dig.id>
2022-04-13 16:13:19 +0100

    up

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -3,4 +3,10 @@ $(function() {
      $('.main-header').toggleClass('is-open');
      $('body').toggleClass('is-open');
   });
+  $('.section-banner__btn').on( 'click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top + -160
+  }, 300);
+  });
 });
--- template-parts/main-footer.php
+++ template-parts/main-footer.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-12 col-lg-5 footer-logo-button">
 				<?php get_template_part( 'template-parts/main-logo' ); ?>
-				<a href="/demo-anfragen/" class="btn btn--demo-request"><?php _e( 'Demo Anfragen', 'bocco-group' ); ?></a>
+				<a href="/demo-anfragen/" class="btn btn--demo-request"><?php _e( 'Anfrage Senden', 'bocco-group' ); ?></a>
 			</div>
 			<div class="col-12 col-lg-7">
 				<div class="row">
