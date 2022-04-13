4b9ddb52f09d7e9c32ab1155f9cd00ff365c59e7
pedro-digid <pedro@dig.id>
2022-04-13 16:32:34 +0100

    up

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+		<meta name="theme-color" content="#080f26" />
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 		<?php wp_head(); ?>
 	</head>
