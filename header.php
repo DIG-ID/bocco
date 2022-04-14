f7367c05dc735647841c0344ec512aea9cb079fd
pedro-digid <pedro@dig.id>
2022-04-14 13:57:32 +0100

    up

--- header.php
+++ header.php
@@ -7,6 +7,15 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<meta name="theme-color" content="#080f26" />
 		<link rel="profile" href="https://gmpg.org/xfn/11">
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-F5VJG2ZFCC"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-F5VJG2ZFCC');
+		</script>
 		<?php wp_head(); ?>
 	</head>
 
