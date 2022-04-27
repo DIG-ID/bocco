efff9e54a1321cc850b1bb850fdb895a723a7b02
pedro-digid <pedro@dig.id>
2022-04-27 15:26:11 +0100

    up

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -9,4 +9,17 @@ $(function() {
       scrollTop: $($.attr(this, 'href')).offset().top + -200
   }, 300);
   });
+  //Back to top button
+  var btnBtt = $('#button-btt');
+  $(window).on( 'scroll', function() {
+    if ($(window).scrollTop() > 300) {
+      btnBtt.addClass('show');
+    } else {
+      btnBtt.removeClass('show');
+    }
+  });
+  btnBtt.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
 });
