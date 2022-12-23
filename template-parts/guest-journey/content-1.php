62b95f138f901823a398244dcdece41099f1f212
Daniel-Digid <daniel@dig.id>
2022-12-23 16:11:50 +0000

    Dev

--- template-parts/guest-journey/content-1.php
+++ template-parts/guest-journey/content-1.php
@@ -1,17 +1,17 @@
-<section id="rmcontent-1" class="section section-rmcontent-1">
+<section id="gjcontent-1" class="section section-gjcontent-1">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-12 col-md-12 col-lg-6 col-xl-6 section-rmcontent-1__leftcol">
-                <div class="section-rmcontent-1__img-pack">
-                    <img class="section-rmcontent-1__img-abtop" src="<?php echo wp_upload_dir()['url'] . '/vorgeschlagene.png' ?>">
-                    <img class="section-rmcontent-1__img-abbot" src="<?php echo wp_upload_dir()['url'] . '/Pre-poststay.png' ?>">
+			<div class="col-12 col-md-12 col-lg-6 col-xl-6 section-gjcontent-1__leftcol">
+                <div class="section-gjcontent-1__img-pack">
+                    <img class="section-gjcontent-1__img-abtop" src="<?php echo wp_upload_dir()['url'] . '/vorgeschlagene.png' ?>">
+                    <img class="section-gjcontent-1__img-abbot" src="<?php echo wp_upload_dir()['url'] . '/Pre-poststay.png' ?>">
                 </div>
 			</div>
-			<div class="col-12 col-md-12 col-lg-6 col-xl-6 section-rmcontent-1__rightcol">
-                <div class="section-rmcontent-1__content-wrapper">
-                    <p class="section-rmcontent-1__subtitle"><?php echo the_field('advantages_subtitle'); ?></p>
-                    <h1 class="section-rmcontent-1__title"><?php echo the_field('advantages_title'); ?></h1>
-                    <ul class="section-rmcontent-1__list">
+			<div class="col-12 col-md-12 col-lg-6 col-xl-6 section-gjcontent-1__rightcol">
+                <div class="section-gjcontent-1__content-wrapper">
+                    <p class="section-gjcontent-1__subtitle"><?php echo the_field('advantages_subtitle'); ?></p>
+                    <h1 class="section-gjcontent-1__title"><?php echo the_field('advantages_title'); ?></h1>
+                    <ul class="section-gjcontent-1__list">
                     <?php
                         if( have_rows('advantages_advantages_list') ):
                             while( have_rows('advantages_advantages_list') ) : the_row(); ?>
--- template-parts/guest-journey/intro.php
+++ template-parts/guest-journey/intro.php
@@ -1,16 +1,16 @@
-<section id="rmintro" class="section section-rmintro">
+<section id="gjintro" class="section section-gjintro">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-12 col-md-12 col-lg-7 col-xl-7 section-rmintro__leftcol">
-                <div class="section-rmintro__content-wrapper">
-                    <p class="section-rmintro__subtitle"><?php echo the_field('overview_subtitle'); ?></p>
-                    <h1 class="section-rmintro__title"><?php echo the_field('overview_title'); ?></h1>
-                    <p class="section-rmintro__text"><?php echo the_field('overview_description'); ?></p>
+			<div class="col-12 col-md-12 col-lg-7 col-xl-7 section-gjintro__leftcol">
+                <div class="section-gjintro__content-wrapper">
+                    <p class="section-gjintro__subtitle"><?php echo the_field('overview_subtitle'); ?></p>
+                    <h1 class="section-gjintro__title"><?php echo the_field('overview_title'); ?></h1>
+                    <p class="section-gjintro__text"><?php echo the_field('overview_description'); ?></p>
                 </div>
 			</div>
 			<div class="col-12 col-md-12 col-lg-5 col-xl-5 section-wbeintro__rightcol">
-                <div class="section-rmintro__img-pack">
-                    <img class="section-rmintro__img-abtop" src="<?php echo wp_upload_dir()['url'] . '/GJT_overview.png' ?>">
+                <div class="section-gjintro__img-pack">
+                    <img class="section-gjintro__img-abtop" src="<?php echo wp_upload_dir()['url'] . '/GJT_overview.png' ?>">
                 </div>
 			</div>
 		</div>
