e770ad36a404cb4efe222a0f30f55a757477b44a
Daniel-Digid <daniel@dig.id>
2024-02-23 15:46:38 +0000

    d

--- template-parts/webinar/loop.php
+++ template-parts/webinar/loop.php
@@ -29,6 +29,14 @@
 			'post_status' => 'publish',
 			'orderby'     => 'date',
 			'order'       => 'ASC',
+			'meta_query'     => array(
+				array(
+					'key'     => 'past_event', // Replace 'field_name' with the name of your ACF field
+					'value'   => '0', // '0' for false, '1' for true
+					'compare' => '==', // '=' for equal to (false)
+					'type'    => 'NUMERIC' // Assuming your field stores 0 or 1 as the value
+				)
+			)
 		);
 
 		$webinar_query = new WP_Query( $webinar_args );
@@ -100,6 +108,96 @@
 			wp_reset_postdata();
 		endif;
 		?>
+		
+		<div class="row">
+			<div class="col-10">
+				<h2><?php esc_html_e( 'Vergangene Events', 'bocco-group' ); ?></h2>
+			</div>
+		</div>
+		<?php
+		$webinar_args = array(
+			'post_type'   => 'webinar',
+			'post_status' => 'publish',
+			'orderby'     => 'date',
+			'order'       => 'ASC',
+			'meta_query'     => array(
+				array(
+					'key'     => 'past_event', // Replace 'field_name' with the name of your ACF field
+					'value'   => '1', // '0' for false, '1' for true
+					'compare' => '==', // '=' for equal to (false)
+					'type'    => 'NUMERIC' // Assuming your field stores 0 or 1 as the value
+				)
+			)
+		);
 
+		$webinar_query = new WP_Query( $webinar_args );
+
+		if ( $webinar_query->have_posts() ) :
+			while ( $webinar_query->have_posts() ) :
+				$webinar_query->the_post();
+				?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'row card-webinar-wrapper' ); ?>>
+					<div class="col-12 col-lg-11">
+						<div class="card-webinar row">
+							<div class="col-12 col-lg-2 text-lg-center">
+								<img class="webinar-thumbnail" src="<?php echo get_template_directory_uri(); ?>/src/images/boccowebinar-post.svg" alt="bocco webinar event image thumbnail">
+							</div>
+							<div class="col-12 col-lg-10">
+								<h3><?php the_title(); ?></h3>
+								<hr>
+								<div class="card-webinar-details row">
+									<div class="col-12 col-lg-4">
+										<p class="card-webinar-details--item">
+											<span><?php esc_html_e( 'Datum:', 'bocco-group' ); ?></span><br>
+											<?php the_field( 'date' ); ?>
+										</p>
+										<p class="card-webinar-details--item">
+											<span><?php esc_html_e( 'Zeit:', 'bocco-group' ); ?></span><br>
+											<?php the_field( 'duration' ); ?>
+										</p>
+										<?php
+										if ( get_field( 'have_host' ) ) :
+											?>
+											<p class="card-webinar-details--item">
+												<span><?php esc_html_e( 'Host:', 'bocco-group' ); ?></span><br>
+												<?php the_field( 'host' ); ?>
+											</p>
+											<?php
+										endif;
+										?>
+									</div>
+									<div class="col-12 col-lg-8 card-webinar-details--description">
+										<div class="card-webinar-details--item mb-2">
+											<span><?php esc_html_e( 'Inhalt:', 'bocco-group' ); ?></span><br>
+											<?php the_content(); ?>
+										</div>
+
+										<?php
+										$card_link = get_field( 'youtube_link' );
+										if ( $card_link ) :
+											$link_url    = $card_link['url'];
+											$link_title  = $card_link['title'];
+											$link_target = $card_link['target'] ? $card_link['target'] : '_self';
+											?>
+											<a class="card-webinar-details--button youtube-btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
+											<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16" fill="none">
+												<path d="M1.61723 16C1.34857 15.9995 1.08468 15.9295 0.851494 15.797C0.32642 15.5013 0 14.9274 0 14.3044V1.69572C0 1.07094 0.32642 0.49877 0.851494 0.203119C1.09023 0.0667878 1.36143 -0.00332087 1.63677 0.000120885C1.9121 0.00356264 2.18144 0.0804287 2.41665 0.222685L13.2616 6.67309C13.4876 6.81391 13.6739 7.00946 13.8031 7.24141C13.9322 7.47335 14 7.73409 14 7.99917C14 8.26425 13.9322 8.52499 13.8031 8.75693C13.6739 8.98888 13.4876 9.18443 13.2616 9.32525L2.4149 15.7774C2.17419 15.922 1.8985 15.9989 1.61723 16Z" fill="#EDF1FD"/>
+											</svg>
+												<?php echo esc_html( $link_title ); ?>
+											</a>
+											<?php
+										endif;
+										?>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</article>
+				<?php
+			endwhile;
+			wp_reset_postdata();
+		endif;
+		?>
 	</div>
 </section>
